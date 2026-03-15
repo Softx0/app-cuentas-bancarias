605060aae1af7fed5f95f3c8b795c2bfa47a20ea
ing.valen0421@hotmail.com <ing.valen0421@hotmail.com>
2026-03-15 07:37:47 -0400

    refact(aws): fine tune the configuration of amplify

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { registerRootComponent } from 'expo';
 import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
-import * as cdk from 'aws-cdk-lib';
 
+import { registerRootComponent } from 'expo';
 import App from './App';
+
 registerRootComponent(App);
